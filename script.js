397f28fc5da49f428344cbb97a865d1cb8eb499a
Nyashadzashe T Katsidzira <98623567+NyashaKays@users.noreply.github.com>
2023-07-26 14:08:17 +0200

    Nearing completion of Desktop site

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 window.onload = function(){
+    window.addEventListener('scroll', function (e) {
+        if(window.pageYOffset > 250){
+            this.document.getElementById('totop').classList.add('active');
+        } else{
+            this.document.getElementById('totop').classList.remove('active');
+        }})
+
+    document.getElementById('totop').addEventListener("click", function(){
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+
+        });
+    });
+
+
+
+
+
+
+
+
+
+
     var text = ["Web Developer!", "Graphic Designer!", "Photographer!"];
     var counter = 0;
     var elem = document.getElementById("textCycle");
@@ -12,20 +37,8 @@ window.onload = function(){
         }
     }
 
-    window.addEventListener('scroll', function (e) {
-        if(window.pageYOffset > 250){
-            this.document.getElementById('totop').classList.add('active');
-        } else{
-            this.document.getElementById('totop').classList.remove('active');
-        }})
-
-    document.getElementById('totop').addEventListener("click", function(){
-        window.scroll({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        });
-    });
+    var i = 0;
+    
 }
 
 
